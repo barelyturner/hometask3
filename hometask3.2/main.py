14b84d9ab4d5fbbea033c84de4313fcb0a8284a4
wasilii <incredibleturner@gmail.com>
2023-07-29 20:19:56 +0300

    edited

--- hometask3.2/main.py
+++ hometask3.2/main.py
@@ -1,3 +1,6 @@
-list1 = [1, "w", 3, 4, 5, [5, 5, 5]]
-list1.insert(0, list1.pop(-1))
-print(list1)
+list1 = []
+if list1 == []:
+    print(list1)
+else:
+    list1.insert(0, list1.pop(-1))
+    print(list1)
